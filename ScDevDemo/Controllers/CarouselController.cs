6109e324ab6e64ddb01feefe1538a518fc103d72
bhargav <bhargav@mirabeau.nl>
2019-03-18 12:05:10 +0530

    Add project files.

---
+++ ScDevDemo/Controllers/CarouselController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using Glass.Mapper.Sc.Web.Mvc;
+using ScDevDemo.Models;
+using Sitecore.Mvc.Presentation;
+
+namespace ScDevDemo.Controllers
+{
+    public class CarouselController : GlassController
+    {
+        // GET: Carousel
+            public ActionResult GetCarousel()
+            {
+                CarouselData carouselModel = new CarouselData();
+                Sitecore.Collections.ChildList slideItems = null;
+                //Get the current context, then datasource item
+                var currentContext = RenderingContext.Current;
+                var currentRendering = currentContext.Rendering;
+                var database = Sitecore.Context.Database;
+                var dataSourceId = currentRendering.DataSource;
+                //Create list of slide items underneath datasource item
+                if (database.GetItem(dataSourceId) != null)
+                {
+                    var dataSource = database.GetItem(dataSourceId);
+                    //Ideally you'd actually loop through these to verify against Slide template ID and populate a separate list then bind that to model object
+                    slideItems = dataSource.GetChildren();
+                }
+                carouselModel.SlideItems = slideItems;
+                return View("~/Views/Components/Carousel/Index.cshtml", carouselModel);
+            }
+    }
+}
---
+++ ScDevDemo/Models/CarouselData.cs
@@ -0,0 +1,7 @@
+﻿namespace ScDevDemo.Models
+{
+    public class CarouselData
+    {
+        public Sitecore.Collections.ChildList SlideItems { get; set; }
+    }
+}
